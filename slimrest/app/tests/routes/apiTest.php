649f6badc8e711da9b70571f404532e914722198
SrLombarda <vavoom02@gmail.com>
2015-01-28 19:40:57 +0100

    red

--- slimrest/app/tests/routes/apiTest.php
+++ slimrest/app/tests/routes/apiTest.php
@@ -2,7 +2,18 @@
 
 	class apiTest extends \PHPUnit_Framework_TestCase
 	{
+	   	public function testPostExpenseReturnsOkIfConnectionIsOk()
+	   	{
+	   		$expected_answer = "OK";
 
+		    $curl_handler = curl_init( "http://localhost:1000/slimrest/expenses" );
+	        curl_setopt( $curl_handler, CURLOPT_RETURNTRANSFER, true );
+	        curl_setopt( $curl_handler, CURLOPT_CUSTOMREQUEST, "POST" );
+	        $response = curl_exec( $curl_handler );
+	        curl_close( $curl_handler );
+			
+   			$this->assertEquals( $expected_answer, $response );
+	    }
 	}
 
 ?>
