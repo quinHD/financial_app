a29428992facf3662e743b3282e2ea140da2a440
SrLombarda <vavoom02@gmail.com>
2015-01-29 02:38:25 +0100

    green

--- slimrest/app/routes/api.php
+++ slimrest/app/routes/api.php
@@ -4,15 +4,22 @@
 		$description = $app -> request -> post( "description" );
 		$amount		 = $app -> request -> post( "amount" );
 
-		$connection = getConnection();
-		$db_handler = $connection -> prepare( "INSERT INTO expenses VALUES( null, ?, ?, NOW() )" );
-		$db_handler -> bindParam( 1, $description );
-		$db_handler -> bindParam( 2, $amount );
-		$db_handler -> execute();
-		$expense = $connection -> lastInsertId();
-		$connection = null;
+		if( isset( $description ) && isset( $amount ) ) 
+		{
+			$connection = getConnection();
+			$db_handler = $connection -> prepare( "INSERT INTO expenses VALUES( null, ?, ?, NOW() )" );
+			$db_handler -> bindParam( 1, $description );
+			$db_handler -> bindParam( 2, $amount );
+			$db_handler -> execute();
+			$expense = $connection -> lastInsertId();
+			$connection = null;
 
-		$app -> response -> body( json_encode( array("answer" => "OK", "content" => $expense )));
+			$app -> response -> body( json_encode( array( "answer" => "OK", "content" => $expense )));
+		}
+		else
+		{
+			$app -> response -> body( json_encode( array( "answer" => -1 )));	
+		}
 		
 	});
 
