f4db0263ebc551c5592a545f2e90515beba0b53c
SrLombarda <vavoom02@gmail.com>
2015-01-29 03:40:35 +0100

    refactor

--- slimrest/app/tests/routes/apiTest.php
+++ slimrest/app/tests/routes/apiTest.php
@@ -8,7 +8,7 @@
 	   		$expected_answer = "OK";
 	   		$minimum_id_value = 1;
 
-		    $response = $this -> execute_http_call( $new_expense );
+		    $response = $this -> execute_http_call( "POST", $new_expense );
 		    
    			$this -> assertEquals( $expected_answer, $response["answer"] );
    			$this -> assertGreaterThanOrEqual( $minimum_id_value, $response["content"] );
@@ -19,7 +19,7 @@
 	   		$new_expense = array( "amount" => 890.95 );
 	   		$expected_answer = -1;
 
-		    $response = $this -> execute_http_call( $new_expense );
+		    $response = $this -> execute_http_call( "POST", $new_expense );
 		    
    			$this -> assertEquals( $expected_answer, $response["answer"] );
 	    }
@@ -28,27 +28,28 @@
 	    {
 	   		$expected_answer = "OK";
 
-		    $curl_handler = curl_init( "http://localhost:1000/slimrest/expenses/1" );
-	        curl_setopt( $curl_handler, CURLOPT_RETURNTRANSFER, true );
-	        curl_setopt( $curl_handler, CURLOPT_CUSTOMREQUEST, "GET" );
-	        $response = curl_exec( $curl_handler );
-	        curl_close( $curl_handler );
+	   		$response = $this -> execute_http_call( "GET", null );
 
-	        $response_decoded = json_decode( $response, true );
-
-   			$this->assertEquals( $expected_answer, $response_decoded["answer"] ); 
+   			$this->assertEquals( $expected_answer, $response["answer"] ); 
 	    }
 
-	    private function execute_http_call( $new_expense=null )
+	    private function execute_http_call( $http_verb, $new_expense )
 	    {
-		    $curl_handler = curl_init( "http://localhost:1000/slimrest/expenses" );
+	    	if($http_verb == "GET")
+	    	{
+	    		$resource = "http://localhost:1000/slimrest/expenses/1";
+	    	}
+	    	else
+	    	{
+	    		$resource = "http://localhost:1000/slimrest/expenses";
+	    	}
+		    $curl_handler = curl_init( $resource );
 	    	curl_setopt( $curl_handler, CURLOPT_RETURNTRANSFER, true );
-	        curl_setopt( $curl_handler, CURLOPT_CUSTOMREQUEST, "POST" );
+	        curl_setopt( $curl_handler, CURLOPT_CUSTOMREQUEST, $http_verb );
 	        if( $new_expense )
 	        {
 	        	curl_setopt( $curl_handler, CURLOPT_POSTFIELDS, http_build_query( $new_expense ));
 	        }
-	        
 	        $response = curl_exec( $curl_handler );
 	        curl_close( $curl_handler );
 
