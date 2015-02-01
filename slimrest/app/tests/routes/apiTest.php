1e7c8428ae7751aff33b38de0c955d332538aff8
SrLombarda <vavoom02@gmail.com>
2015-02-01 04:28:14 +0100

    Revert "refactor"
    This reverts commit 846f2ab12114d25e9387c2ad5126f011eb0efd71.

--- slimrest/app/tests/routes/apiTest.php
+++ slimrest/app/tests/routes/apiTest.php
@@ -122,8 +122,15 @@
 	    {
 	   		$expected_answer = "OK";
 
-	        $response = $this -> execute_http_call( "GET", null );
-	        $answer_received = $response[ "answer" ];
+		    $curl_handler = curl_init( "http://localhost:1000/slimrest/expenses/" );
+	        curl_setopt( $curl_handler, CURLOPT_RETURNTRANSFER, true );
+	        curl_setopt( $curl_handler, CURLOPT_CUSTOMREQUEST, "GET" );
+	        $response = curl_exec( $curl_handler );
+	        curl_close( $curl_handler );
+
+	        $response_decoded = json_decode( $response, true );
+	        
+	        $answer_received = $response_decoded[ "answer" ];
 			
    			$this -> assertEquals( $expected_answer, $answer_received ); 
 	    }
